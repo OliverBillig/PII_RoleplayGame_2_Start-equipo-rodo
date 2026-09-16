47845f55cc0668f14bfcfb7bd0cd4dc9b92727c3
MatiCHad <matiashernandezd21@gmail.com>
2026-09-16 08:34:35 -0300

    Clase PJ para empezar herencia

---
+++ src/Library/Characters/PJ.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace Ucu.Poo.RoleplayGame;
+
+public class PJ
+{
+    private int health = 100;
+
+    public PJ(string name)
+    {
+        this.Name = name;
+    }
+
+    public string Name { get; set; }
+    
+    public int AttackValue { get; set; }
+
+    public int DefenseValue { get; set; }
+
+    public int Health
+    {
+        get
+        {
+            return this.health;
+        }
+        private set
+        {
+            this.health = value < 0 ? 0 : value;
+        }
+    }
+
+    public void ReceiveAttack(int power)
+    {
+        if (this.DefenseValue < power)
+        {
+            this.Health -= power - this.DefenseValue;
+        }
+    }
+
+    public void Cure()
+    {
+        this.Health = 100;
+    }
+}
